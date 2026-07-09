036140ccdc86ba9e63dda89114ece5960802cf22
Kyle T <kyletruong2000@gmail.com>
2026-07-09 13:53:10 -0700

    Added 3 inputs to Plan.tsx that uses useRef to set it's value.

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,8 +1,40 @@
-function Plan() {
+import { useState, useRef } from 'react'
+import Button from '../components/Button.tsx'
 
+function Plan() {
+    const address = useRef<string>("");
+    const city = useRef<string>("");
+    const country = useRef<string>("");
+    // const [address, setAddress] = useState<string>("");
+    // const [city, setCity] = useState<string>("");
+    // const [country, setCountry] = useState<string>("");
     return(
         <>
-            <h1>Work in Progress</h1>
+            <form>
+                <input 
+                    type="text"
+                    required
+                    className="addressInput"
+                    onChange={(e) => address.current = e.target.value}
+                    placeholder="Address"
+                />
+
+                <input
+                    type="text"
+                    required
+                    className="cityInput"
+                    onChange={(e) => city.current = e.target.value}
+                    placeholder ="City"
+                />
+
+                <input
+                    type="text"
+                    required
+                    className="countryInput"
+                    onChange={(e) => country.current = e.target.value}
+                    placeholder ="Country"
+                />
+            </form>
         </>
     )
 }
